0035288c2ed440bbfdb15c4f92b0d8a78e934bd4
Vrushabh <vrushabh0078@gmail.com>
2024-05-28 15:26:45 +0530

    new changes

--- src/Components/Menu/MenuApi.js
+++ src/Components/Menu/MenuApi.js
@@ -7,7 +7,7 @@ const Menu = [
         name: "Maggie",
         category: "Breakfast",
         price: "12Rs",
-        description: "Maggie is so Tasty",  
+        description: "Maggie is a dynamic and enthusiastic individual known for her creativity and positive outlook. Her natural leadership and empathy make her a beloved figure in both her personal and professional circles.",  
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ const Menu = [
         name: "AlluPakoda",
         category: "Evening",
         price: "20Rs",
-        description: "AlluPakoda is so Tasty",  
+        description: "AlluPakoda is a popular Indian snack made from thinly sliced potatoes coated in a flavorful chickpea flour batter and deep-fried to crispy perfection. It's a crunchy and irresistible treat enjoyed across the country.",  
     },
     {
         id: 3,
@@ -23,7 +23,7 @@ const Menu = [
         name: "Burger",
         category: "Evening",
         price: "30Rs",
-        description: "Burger is so Tasty",  
+        description: "A burger is a classic American sandwich consisting of a grilled or fried patty made from ground beef or alternative proteins, served between two halves of a soft bun. It's a beloved fast-food staple worldwide, customizable with various toppings like cheese, lettuce, tomato, and sauces.",  
     },
     {
         id: 4,
@@ -31,7 +31,7 @@ const Menu = [
         name: "Chole",
         category: "Lunch",
         price: "50Rs",
-        description: "Chole is so Tasty",  
+        description: "Chole, also known as chana masala, is a popular North Indian dish made from chickpeas cooked in a spicy and aromatic tomato-based gravy. It's often served with fluffy bhature or rice, delighting taste buds with its rich flavor and hearty texture.",  
     },
     {
         id: 5,
@@ -39,7 +39,7 @@ const Menu = [
         name: "Pizza",
         category: "Evening",
         price: "120Rs",
-        description: "Pizza is so Tasty",  
+        description: "Pizza is an iconic Italian dish featuring a thin or thick crust topped with tomato sauce, cheese, and various ingredients like pepperoni, mushrooms, or vegetables. Loved worldwide for its comforting and customizable flavors, it's a staple of casual dining and social gatherings.",  
     },
     {
         id: 6,
@@ -47,7 +47,7 @@ const Menu = [
         name: "Manchurian",
         category: "Evening",
         price: "60Rs",
-        description: "Manchurian is so Tasty",  
+        description: "Manchurian is a savory Indo-Chinese dish made from vegetable or meatballs coated in a tangy and spicy sauce, typically featuring garlic, ginger, soy sauce, and chili. With its crispy exterior and flavorful sauce, it's a popular appetizer or main course, enjoyed for its fusion of Indian and Chinese flavors.",  
     },
     {
         id: 7,
@@ -55,7 +55,7 @@ const Menu = [
         name: "Khichdi",
         category: "Dinner",
         price: "50Rs",
-        description: "Khichdi is so Tasty",  
+        description: "Khichdi is a comforting and nutritious Indian dish made from rice and lentils cooked together with aromatic spices like cumin, turmeric, and ginger. It's a simple yet hearty meal, often served with yogurt, pickles, or vegetables, enjoyed for its soothing flavor and easy digestibility.",  
     },
     {
         id: 8,
@@ -63,7 +63,7 @@ const Menu = [
         name: "Sweets",
         category: "Dinner",
         price: "10Rs",
-        description: "Sweets is so Tasty",  
+        description: "Sweets encompass a wide range of delectable treats, from delicate pastries to rich chocolates and traditional confections like gulab jamun and jalebi. These indulgent delights tantalize taste buds with their sweetness, bringing joy and celebration to any occasion.",  
     },
     {
         id: 9,
@@ -71,7 +71,7 @@ const Menu = [
         name: "Rajma Rice",
         category: "Dinner",
         price: "100Rs",
-        description: "Rajma Chawal is so Tasty",  
+        description: "Rajma Chaval is a beloved North Indian dish consisting of kidney beans cooked in a flavorful tomato-based gravy, served alongside steamed rice. This hearty and comforting meal is cherished for its rich, spicy flavors and nutritious ingredients, making it a favorite comfort food for many.",  
     },
     {
         id: 10,
@@ -79,7 +79,7 @@ const Menu = [
         name: "Samosa",
         category: "Breakfast",
         price: "30Rs",
-        description: "Samosa is so Tasty",  
+        description: "Samosa is a popular Indian snack featuring a crispy, triangular pastry shell filled with a savory mixture of spiced potatoes, peas, and sometimes meat. Deep-fried to golden perfection, it's enjoyed across cultures for its crunchy exterior and flavorful, aromatic filling.",  
     },
 
 ]
--- src/Components/Menucard/Menucard.jsx
+++ src/Components/Menucard/Menucard.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Menucard.css'
 
 const Menucard = ( { menuData} ) => {
+
+    const [ visibleDesc , setVisibleDesc ] = useState({})
+
+    const toggleDesc = ( itemId ) => {
+        setVisibleDesc ({
+            ...visibleDesc,
+            [itemId] : !visibleDesc[itemId]
+        })
+    }
+    
   return (
     
         <section className='main-card--container'>
@@ -11,19 +21,22 @@ const Menucard = ( { menuData} ) => {
                         <div className="card-container" key={curElem.id}>
                             <div className="card">
                                 <div className="card-body">
-                                    <span className='card-number card circle subtle'>
-                                        {curElem.id}
-                                    </span>
                                     <span className='card-author subtle'>
-                                        {curElem.name}
+                                    {curElem.id}.{curElem.name}
                                     </span>
                                     <h2 className='card-title'>
                                         {curElem.name}
                                     </h2>
-                                    <span className='card-description subtle'>
-                                        {curElem.description}
-                                    </span>
-                                    <div className="card-read">Read</div>
+                                    { visibleDesc [curElem.id] ? (
+                                        <span className='card-description subtle'>
+                                            {curElem.description}
+                                        </span>
+                                    ) : null 
+                                    }
+                                    <br /><br />
+                                    <div className="card-read" onClick={() => toggleDesc(curElem.id)}>
+                                        {visibleDesc[curElem.id] ? 'Hide' : 'Read'}
+                                    </div>
                                 </div>
                                 <img src={curElem.img} alt="images" className='card-media' />
                                 <span className="card-tag subtle">Order Now</span>
